3899e3c7a2edb29cd6ccf31408e47578bc0da67f
Aye Myat Thin aung <ayemyatthin.aung@hiveinnovate.com>
2022-03-26 01:18:24 +0630

    change add-to-cart btn with item qty increase decrease feature

--- frontend/src/components/button/AddToCartButton.jsx
+++ frontend/src/components/button/AddToCartButton.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 import { toast } from "react-toastify";
+import { useCart } from "../../contexts/cart/cart.context";
+import CounterButton from "./CounterButton";
 
 const AddToCartButton = ({ item }) => {
+  const { addItemToCart, getItemFromCart, decreaseItemQuantity, isInCart } =
+    useCart();
+
   return (
     <>
-      <button
-        type="button"
-        className={`${
-          !item?.stock && "disabled"
-        } btn bg-asset1 w-100 text-white`}
-        disabled={!item?.stock}
-        onClick={() => {
-          toast.success("One item is added to the cart.");
-        }}
-      >
-        ADD TO CART
-      </button>
+      {!isInCart(item?.id) ? (
+        <button
+          type="button"
+          className={`${
+            !item?.stock && "disabled"
+          } btn bg-asset1 w-100 text-white`}
+          disabled={!item?.stock}
+          onClick={() => {
+            addItemToCart(item, 1);
+            toast.success("One item is added to the cart.");
+          }}
+        >
+          ADD TO CART
+        </button>
+      ) : (
+        <CounterButton
+          quantity={getItemFromCart(item.id)?.quantity}
+          // decrementDisabled={getItemFromCart(item.id)?.quantity === 1}
+          incrementDisabled={getItemFromCart(item.id)?.quantity >= item?.stock}
+          onDecrement={() => {
+            decreaseItemQuantity(item.id);
+            toast.success("One item is removed from the cart.");
+          }}
+          onIncrement={() => {
+            addItemToCart(item, 1);
+            toast.success("One item is added to the cart.");
+          }}
+        />
+      )}
     </>
   );
 };
