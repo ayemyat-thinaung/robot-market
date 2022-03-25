075b403e548903466d7b764f356bd66cf4ed5472
Aye Myat Thin aung <ayemyatthin.aung@hiveinnovate.com>
2022-03-25 23:35:19 +0630

    add app layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
 
 const App = () => {
   return (
     <Router>
       <div className="d-flex flex-column justify-content-between min-vh-100">
+        <Navbar />
         <main>
           <Switch>
             <Route exact path="/">
@@ -17,7 +20,7 @@ const App = () => {
             </Route>
           </Switch>
         </main>
-
+        <Footer />
         <ToastContainer
           position="bottom-right"
           autoClose={1500}
