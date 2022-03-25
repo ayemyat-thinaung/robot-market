444c439068248ef71afe8ce561b849e9a0afce62
Aye Myat Thin aung <ayemyatthin.aung@hiveinnovate.com>
2022-03-25 23:38:29 +0630

    add css files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./assets/scss/main.scss";
+import "react-toastify/dist/ReactToastify.css"; //external css
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
