0cb56a668fb70cbd6f6ba8cef1e9dddb50ab3b73
Aye Myat Thin aung <ayemyatthin.aung@hiveinnovate.com>
2022-03-26 15:34:22 +0630

    edit grid col and text color

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -2,52 +2,64 @@ import React from "react";
 
 const Footer = () => (
   <footer>
-    {/* <div className="bg-asset3 py-5 px-4">
-      <div className="container "> */}
     <div className="py-5">
       <div className="container rounded-default bg-asset3 p-5">
         <div className="row gx-5">
-          <div className="col-3">
-            <h4 className="text-asset1">Robot Market</h4>
-            <p className="text-break">
+          <div className="col-md-6 col-lg-6 col-xl-3">
+            <h4 className="text-asset1 ls-1">Robot Market</h4>
+            <p className="text-break text-secondary">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. t
-              deleniti? Iure ipsa cupiditate fugit cum.
+              deleniti? Iure ipsa.
             </p>
           </div>
 
-          <div className="col-2">
+          <div className="col-md-6 col-lg-6 col-xl-2">
             <h5>About Us</h5>
-            <ul className="list-unstyled ">
+            <ul className="list-unstyled">
               <li>
-                <a href="/">Home</a>
+                <a href="/" className="text-secondary">
+                  Home
+                </a>
               </li>
               <li>
-                <a href="/products">Our Products</a>
+                <a href="/products" className="text-secondary">
+                  Our Products
+                </a>
               </li>
               <li>
-                <a href="/contact-us">Contact Us</a>
+                <a href="/contact-us" className="text-secondary">
+                  Contact Us
+                </a>
               </li>
             </ul>
           </div>
-          <div className="col-2">
+          <div className="col-md-6 col-lg-6 col-xl-2">
             <h5>Customer Care</h5>
             <ul className="list-unstyled ">
               <li>
-                <a href="/">FAQs</a>
+                <a href="/" className="text-secondary">
+                  FAQs
+                </a>
               </li>
               <li>
-                <a href="/">Help Center</a>
+                <a href="/" className="text-secondary">
+                  Help Center
+                </a>
               </li>
               <li>
-                <a href="/">Returns & Refunds</a>
+                <a href="/" className="text-secondary">
+                  Returns & Refunds
+                </a>
               </li>
               <li>
-                <a href="/">Terms & Conditions</a>
+                <a href="/" className="text-secondary">
+                  Terms & Conditions
+                </a>
               </li>
             </ul>
           </div>
 
-          <div className="col">
+          <div className="col-md-6 col-lg-6 col-xl-5">
             <h5>Subscribe for Product Updates</h5>
             <div className="input-group my-4">
               <input
